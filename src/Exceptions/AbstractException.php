125e370d293897d235f57346357fc0795f539a1f
ueef <artem@arkadev.org>
2019-06-10 12:45:46 +0500

    add exceptions 2

--- src/Exceptions/AbstractException.php
+++ src/Exceptions/AbstractException.php
@@ -10,6 +10,16 @@ class AbstractException extends Exception
 {
     public function __construct($message = "", Throwable $previous = null)
     {
+        if (is_array($message)) {
+            foreach ($message as &$value) {
+                if (is_array($value) || is_object($value)) {
+                    $value = json_encode($value, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+                }
+            }
+
+            $message = sprintf(...$message);
+        }
+
         parent::__construct($message, 0, $previous);
     }
 }
